0830cb2336595a38c1a94bede3dd51ebbca8caa6
Neo <nnspecter@gmail.com>
2025-12-04 13:58:40 +0500

    переадресация с главной страницы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 'use client'
 
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 
 const Page = () => {
+  const router = useRouter();
+  useEffect(()=>{
+    router.push("/login")
+  })
+  
   return (
     <div>
       
