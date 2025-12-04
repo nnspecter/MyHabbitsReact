62f4eff96eb470b9e649843217a0b6bf8d598fa9
Neo <nnspecter@gmail.com>
2025-12-04 14:48:43 +0500

    пробую исправить хедер

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Header.module.scss';
+import Link from 'next/link';
 
 const Header = () => {
   return (
@@ -10,11 +11,11 @@ const Header = () => {
         </div>
         <nav className={styles.nav}>
           <ul>
-            <li><a href="/table">Таблица</a></li>
-            <li><a href="/dashboard">Дэшборд</a></li>
-            <li><a href="/settings">Настройки</a></li>
-            <li><a href="#contact">Сводка</a></li>
-            <li><a href="#contact">Профиль</a></li>
+            <li><Link href="/table">Таблица</Link></li>
+            <li><Link href="/dashboard">Дэшборд</Link></li>
+            <li><Link href="/settings">Настройки</Link></li>
+            <li><Link href="#contact">Сводка</Link></li>
+            <li><Link href="#contact">Профиль</Link></li>
           </ul>
         </nav>
       </div>
